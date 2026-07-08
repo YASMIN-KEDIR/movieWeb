11bae488020d064e2a14292f838543860b6d8867
yasmin kedir <yasmina.k1781@gmail.com>
2026-07-08 04:54:08 -0700

    feat: update homepage UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import { useEffect, useState} from 'react'
+import Search from './component/Search'
 
 const App = () => {
+  const [searchTerm, setSearchTerm] =useState(' ');
   return (
     <main>
       <div className="pattern"/>
       <div className="wrapper"> 
  <header>
-  <h1>find <span claaName="text-gradient"> islamic kids</span> entertainment with knowledge</h1>
+  <img src="./hero-img.png" alt="Hero-banner" />
+  <h1  className=" flex items-center justify-center">Find <span className="text-gradient"> movies</span> you'll Enjoy Without The Hassel</h1>
  </header>
- <p> Search</p>
+  <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       </div>
     </main>
   )
